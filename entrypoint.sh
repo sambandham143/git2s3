be5e64b658a7f20d0898db8536700dcab4029781
Sambandham <ss080999@cerner.com>
2021-03-15 08:27:38 +0530

    changed the endpoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,7 +30,7 @@ fi
 # Create a dedicated profile for this action to avoid conflicts
 # with past/future actions.
 
-aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+aws configure  <<-EOF > /dev/null 2>&1
 ${AWS_ACCESS_KEY_ID}
 ${AWS_SECRET_ACCESS_KEY}
 ${AWS_REGION}
@@ -40,7 +40,6 @@ EOF
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
 sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
-              --profile s3-sync-action \
               --no-progress \
               ${ENDPOINT_APPEND} $*"
 
@@ -48,7 +47,7 @@ sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
 # We need to re-run `aws configure` with bogus input instead of
 # deleting ~/.aws in case there are other credentials living there.
 # https://forums.aws.amazon.com/thread.jspa?threadID=148833
-aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+aws configure  <<-EOF > /dev/null 2>&1
 null
 null
 null
